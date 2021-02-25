b97b39067c5f13f22cf2d12538a62470dd15df74
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-02-24 20:52:14 -0600

    Started express work

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-import express from 'express';
+import * as express from "express";
+import * as redis from "redis";
+import * as compression from "compression";
+import * as path from "path";
+
+//* Initalizes express and config
+// express init
+const app = express();
+// defines port
+const port = 5000;
+
+//* Middleware
+// adds 'smart' compression
+app.use(compression());
+// static files
+app.use("/static", express.static(path.join(__dirname, "../static")));
+
+//* Routes
+app.get("/*", (req, res) => {
+  res.send("Hello, world!");
+});
+
+//* Listening
+// tells electron to listen to predefined port
+app.listen(port, "localhost", () => {
+  // notifys express is listening and which port it is
+  console.log(`Listening on port ${port}!`);
+});
