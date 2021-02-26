757c7ebdb0c8106c4c460994a819ddce9c28a46f
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-02-25 21:23:49 -0600

    added index.html servering and script injection

--- index.ts
+++ index.ts
@@ -2,12 +2,31 @@ import * as express from "express";
 import * as redis from "redis";
 import * as compression from "compression";
 import * as path from "path";
+import { promises } from "fs";
 
 //* Initalizes express and config
 // express init
 const app = express();
 // defines port
 const port = 5000;
+//html string
+let htmlStr =
+  "<h1>Server Error (htmlStr not updated from start up). Try reloading!</h1>";
+let timeLastUpdated: number = Date.now();
+let updateHtmlStr = async (force = false) => {
+  if (force || Date.now() < timeLastUpdated + 15000) {
+    timeLastUpdated = Date.now();
+    let tempHtmlStr = (
+      await promises.readFile("./build/index.html")
+    ).toString();
+    tempHtmlStr = tempHtmlStr.replace(
+      "/*script-inject*/",
+      (await promises.readFile("./build/elm.min.js")).toString()
+    );
+    htmlStr = tempHtmlStr;
+  }
+};
+updateHtmlStr(true);
 
 //* Middleware
 // adds 'smart' compression
@@ -16,8 +35,11 @@ app.use(compression());
 app.use("/static", express.static(path.join(__dirname, "../static")));
 
 //* Routes
-app.get("/*", (req, res) => {
-  res.send("Hello, world!");
+app.get("/*", async (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(htmlStr.replace("/*flags-inject*/", "")); //TODO
+  res.end();
+  updateHtmlStr();
 });
 
 //* Listening
