8c277f6383d690f375fbc90cb0450982c965d46d
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-03-03 21:12:09 -0600

    First steps of moving from buildSchema to GraphQLSchema

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,42 +1,89 @@
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
+import {
+  buildSchema,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} from "graphql";
+
+//
+const Planner = new GraphQLObjectType({
+  name: "Planner",
+  fields: {
+    id: {
+      type: GraphQLString,
+      resolve: (source) => {
+        return source;
+      },
+    },
+  },
+});
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    serverMsg: String,
-    planner(id: String!): Planner,
-  }
-  type Planner {
-    id: String!,
-    name: String!,
-    description: String,
-    adminHashHex: String,
-    refreshTimeHourUTC: Int!,
-    plannerLengthHours: Int!,
-    timeIncrementSeconds: Int!,
-    planned: [Booking]
-  }
-  type Booking {
-    startSeconds: Int!,
-    endSeconds: Int!,
-    count: Int!,
-    names: [String]
-  }
-`);
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+      serverMsg: {
+        type: GraphQLString,
+        resolve: () => {
+          return "Hello world!";
+        },
+      },
+      planner: {
+        type: Planner,
+        args: {
+          id: {
+            type: GraphQLString,
+          },
+        },
+        resolve: (source, arg) => {
+          return arg.id;
+        },
+      },
+    },
+  }),
+});
+// var schema = buildSchema(`
+//   type Query {
+//     serverMsg: String,
+//     planner(id: String!): Planner
+//   }
+//   type Mutation {
+//     CreatePlanner(id: String, name: String) {
+//       id: String,
+//       name: String,
+//       secret: String,
+//       adminHashHex: String
+//     }
+//   }
+//   type Planner {
+//     id: String!,
+//     name: String!,
+//     description: String,
+//     adminHashHex: String,
+//     refreshTimeHourUTC: Int!,
+//     plannerLengthHours: Int!,
+//     timeIncrementSeconds: Int!,
+//     planned: [Booking]
+//   }
+//   type Booking {
+//     startSeconds: Int!,
+//     endSeconds: Int!,
+//     count: Int!,
+//     names: [String]
+//   }
+// `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
-  serverMsg: () => {
-    return "Hello world!";
-  },
-  planner: async (id) => {
-    return null;
-  },
-};
+// var root = {
+//   serverMsg: () => {
+//     return "Hello world!";
+//   },
+//   planner: async (id) => {
+//     return null;
+//   },
+// };
 
-export default graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-});
+export default graphqlHTTP({ schema: schema, graphiql: true });
