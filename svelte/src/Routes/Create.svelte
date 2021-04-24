e104d5d03176d8785da5e85541b443391ba8ec91
Caleb-Irwin <56744087+Caleb-Irwin@users.noreply.github.com>
2021-04-24 11:08:49 -0600

    Create updated for new PlannerInput

--- svelte/src/Routes/Create.svelte
+++ svelte/src/Routes/Create.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import PlannerInput from "../Components/PlannerInput.svelte";
+  let name = "";
+  let id = "";
 </script>
 
 <div>
   <form>
     <!-- <label for="nameInput">Name:</label> -->
-    <PlannerInput onlyValidate={true} />
+    <PlannerInput
+      createMode={true}
+      showValidation={true}
+      bind:textField={name}
+      bind:id
+    />
+    {#if id !== null}
+      <h4>
+        Display Name: {name}<br /> Planner Id: {id}
+      </h4>
+    {/if}
   </form>
 </div>
